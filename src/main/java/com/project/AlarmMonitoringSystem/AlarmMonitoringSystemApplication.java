c59ed45fcd9e626e3fb3cd9928c83d4f06e4a075
YugmaP <40856628+YugmaP@users.noreply.github.com>
2020-04-16 23:16:41 +0530

    Initial commit

---
+++ src/main/java/com/project/AlarmMonitoringSystem/AlarmMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.AlarmMonitoringSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmMonitoringSystemApplication.class, args);
+	}
+
+}
