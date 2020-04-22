22e7236acacfc87ba5380eb03c3d3cc687b9a9a9
YugmaP <40856628+YugmaP@users.noreply.github.com>
2020-04-22 18:59:50 +0530

    Sample Data schema

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user values(1, 'Yugma Fernando', 'yugma', 'yugma', 'fyugma@gmail.com', '0755172666');
+INSERT INTO user values(2, 'Admin', 'admin', 'admin', 'admin@gmail.com', '0765485454');
+INSERT INTO user values(3, 'Mave Rolland', 'mave', 'mave', 'mave.r@gmail.com', '0765485454');
